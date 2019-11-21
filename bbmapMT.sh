71ce0274aec4c157505a5a6fcaa7515ba6050874
schimar <schimar@gmail.com>
2019-11-21 11:20:34 +0100

    90, not 95

--- bbmapMT.sh
+++ bbmapMT.sh
@@ -14,7 +14,7 @@ fq2=( fq/460*/460*R2*.fq )
 for i in {0..16}; do
 	id=$( echo ${fq1[$i]} | cut -f1 -d'.' | cut -f3 -d'/' | cut -f1,2 -d'_' )
 	echo $id
-	sysconfcpus -n $ncpus "${bbpth}"bbmap.sh -Xmx110g t=12 ref=bbmap/mt/92010/92010_songxin.fa in1=${fq1[$i]} in2=${fq2[$i]} out=bbmap/mt/92010/id95/${id}.sam minid=0.95 rgid=$id rglb=igaRNA rgsm=$id rgpl=ILLUMINA
+	sysconfcpus -n $ncpus "${bbpth}"bbmap.sh -Xmx110g t=12 ref=bbmap/mt/92010/92010_songxin.fa in1=${fq1[$i]} in2=${fq2[$i]} out=bbmap/mt/92010/id90/${id}.sam minid=0.90 rgid=$id rglb=igaRNA rgsm=$id rgpl=ILLUMINA
 
 done
 
