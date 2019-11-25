17965abf4c04de0960fd9140f46a88b96f4e5270
schimar <schimar@gmail.com>
2019-11-25 10:52:14 +0100

    gatk for 3x pallida

--- gatkRNA.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-module load jdk/1.8.0_45
-module load python/3.6.3
-
-
-java -Xmx256g -jar ${gatkPth}gatk-package-4.1.2.0-local.jar HaplotypeCaller -R ref/Mpallida_transcriptome_final.fasta -mbq 17 -I mybbam.bam -O mpal17rna.vcf 
-
